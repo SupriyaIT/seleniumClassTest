bf1dbded254b7545601fe25c7ef4d22c7d0ab5ac
SUPRIYA <SUPRIYA@SUPRIYA-PC>
2022-09-20 22:06:23 +0530

    second commit

---
+++ src/GitDemoPackage/Test2.java
@@ -0,0 +1,19 @@
+package GitDemoPackage;
+
+public class Test2 {
+
+	public static void main(String[] args) {
+		String angry="yes";
+		if(angry=="yes")
+		{
+			System.out.println("drink water....");
+		}
+		else
+		{
+			System.out.println("Do testing....");
+		}
+		// TODO Auto-generated method stub
+
+	}
+
+}
