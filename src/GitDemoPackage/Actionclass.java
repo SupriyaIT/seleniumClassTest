0fbc1fe3bb714120f2e0cc6373b92901725e4992
SUPRIYA <SUPRIYA@SUPRIYA-PC>
2022-10-02 21:09:19 +0530

    third commit

---
+++ src/GitDemoPackage/Actionclass.java
@@ -0,0 +1,18 @@
+package GitDemoPackage;
+
+public class Actionclass {
+
+	public static void main(String[] args) {
+		String hungry="yes";
+		if(hungry=="yes")
+		{
+			System.out.println("eat something....");
+		}
+		else
+		{
+			System.out.println("Do testing....");
+		}
+
+	}
+
+}
